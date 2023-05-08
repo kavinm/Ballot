dd52e0ca2ea521c02df46b624597464dc8086c63
kavinm <30965883+kavinm@users.noreply.github.com>
2023-05-08 11:19:08 -0400

    cleaned up deploy script

--- scripts/Deployment.ts
+++ scripts/Deployment.ts
@@ -3,6 +3,8 @@ import { Ballot__factory } from "../typechain-types";
 import * as dotenv from "dotenv";
 dotenv.config();
 
+// run file from terminal with arguments of proposal names: yarn ts-node --files ./scripts/Deployment.ts <proposal[0]> <proposal[1]> <proposal[2]> ...
+
 function convertStringArrayToBytes32(array: string[]) {
   const bytes32Array = [];
   for (let index = 0; index < array.length; index++) {
@@ -11,21 +13,20 @@ function convertStringArrayToBytes32(array: string[]) {
   return bytes32Array;
 }
 
-const ADDRESS = "0x58ebbf51dc43a0398d154c36c4de7c88fe36fe28";
 async function main() {
-  const args = process.argv;
-  const proposals = args.slice(2);
+  const proposals = process.argv.slice(2);
   if (proposals.length < 1) throw new Error("Missing parameters: proposals");
 
   const provider = new ethers.providers.AlchemyProvider(
     "maticmum",
     process.env.ALCHEMY_API_KEY
   );
-  // const provider = ethers.getDefaultProvider("goerli");
   const pkey = process.env.PRIVATE_KEY;
   if (!pkey || pkey.length <= 0) throw new Error("Missing privte key");
   const wallet = new ethers.Wallet(`${pkey}`);
   const signer = wallet.connect(provider);
+  console.log(`Connected to ${provider.network.name}`);
+
   console.log("Proposals: ");
   proposals.forEach((element, index) => {
     console.log(`Proposal N. ${index + 1}: ${element}`);
@@ -36,16 +37,11 @@ async function main() {
     convertStringArrayToBytes32(proposals)
   );
   const deployTx = await ballotContract.deployTransaction.wait();
-  console.log(`The contract was deployed on the Goerli testnet at address: ${ballotContract.address} 
+  console.log(`The contract was deployed on the Polygon Mumbai testnet at address: ${ballotContract.address} 
     at block ${deployTx.blockNumber}`);
   console.log(`Transaction hash: ${deployTx.transactionHash}`);
   const chairperson = await ballotContract.chairperson();
   console.log(`The chairperson is ${chairperson}`);
-
-  console.log(`The voter address is ${ADDRESS}`);
-  const giveRightToVoteTx = await ballotContract.giveRightToVote(ADDRESS);
-  await giveRightToVoteTx.wait();
-  console.log(`the transaction hash is ${giveRightToVoteTx.hash}`);
 }
 
 main().catch((error) => {
